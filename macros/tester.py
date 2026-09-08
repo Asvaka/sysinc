b64639cc72e1e5cdd92f620935e5aa2298376a02
Asvaka <asvaka.varnst@outlook.com>
2026-09-08 12:20:22 -0700

    Started macros assignment

---
+++ macros/tester.py
@@ -0,0 +1,11 @@
+import os
+
+os.system("podman build -t macros .")
+
+print()
+
+command = "gcc " + input('File to compile: ') + " -lm -std=c89 -Wall -Wextra -Werror -Wpedantic -O2; ./a.out " + input('Test input: ')
+
+print()
+
+os.system('podman run macros /bin/bash -c "' + command + '"')
