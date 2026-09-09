1dde9c9df6cc9e7aa5aafdb14ee4b02fde1076db
Asvaka <asvaka.varnst@outlook.com>
2026-09-09 10:26:00 -0700

    Updated printb.c to work with ints as well as char (and theoretically other types as well)

--- macros/printb.c
+++ macros/printb.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
-int printb(char n) {
-	int c = n;
+int printb(int n) {
+	unsigned int c = n;
 	int i;
+	int j;
+	int k = (sizeof(n) * 8) - 1;
 
-	for (i = 7; i >= 0; i--) {
+	for (j = sizeof(n) - 1; j >= 0; j--) {
+		if (!(c >> (j*8))) {
+			k -= 8;	
+		}
+	}
+
+	for (i = k; i >= 0; i--) {
 		int mask = 1 << i;
 
-		printf("%d", (c & mask) >> i);
+		printf("%u", (c & mask) >> i);
 	}
 
 	return 0;
 }
+
